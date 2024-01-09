fd4beb6f7705b3fafe1fe6999ee5b1744ee3ae78
Eli Júnior <elijunior.py@gmail.com>
2024-01-09 11:27:22 -0300

    feat: subtests

--- PROJ3CT/futebol/tests/test_futebol_models.py
+++ PROJ3CT/futebol/tests/test_futebol_models.py
@@ -2,25 +2,51 @@ import pytest
 
 from PROJ3CT.futebol.models import Jogador, Time
 from PROJ3CT.futebol.services.factories import jogador_factory, time_factory
+from PROJ3CT.futebol.services.normalize import padronizar_nome
+
 
 
 @pytest.mark.django_db
-def test_criar_time():
+def test_criar_time(subtests):
     novo_time = time_factory()[0]
-    time = Time.objects.create(nome=novo_time.nome)
-    assert Time.objects.count() == 1
-    assert time.nome == novo_time.nome
-    assert str(time) == novo_time.nome
+    time = Time.objects.create(nome=novo_time.nome, nome_interno=padronizar_nome(novo_time.nome))
+
+    testes = (
+        (Time.objects.count(), 1),
+        (time.nome, novo_time.nome),
+        (str(time), novo_time.nome),
+    )
+
+    for entrada, saida in testes:
+        with subtests.test(msg='Testa os dados do time criado', entrada=entrada, saida=saida):
+            assert entrada == saida
 
 
 @pytest.mark.django_db
-def test_criar_jogador():
+def test_criar_jogador(subtests):
     novo_time = time_factory()[0]
     novo_jogador = jogador_factory()[0]
     time = Time.objects.create(nome=novo_time.nome)
     jogador = Jogador.objects.create(**novo_jogador._asdict(), time=time)
-    assert Jogador.objects.count() == 1
-    assert jogador.nome == novo_jogador.nome
-    assert jogador.idade == novo_jogador.idade
-    assert jogador.time == time
-    assert str(jogador) == novo_jogador.nome
+    
+    testes = (
+        (Jogador.objects.count(), 1),
+        (jogador.nome, novo_jogador.nome),
+        (jogador.idade, novo_jogador.idade),
+        (jogador.time, time),
+        (str(jogador), novo_jogador.nome),
+    )
+    for entrada, saida in testes:
+        with subtests.test(msg='Testa os dados do jogador criado', entrada=entrada, saida=saida):
+            assert entrada == saida
+
+
+@pytest.mark.django_db
+def test_excluir_time_do_jogador_seta_para_nulo():
+    novo_time = time_factory()[0]
+    novo_jogador = jogador_factory()[0]
+    time = Time.objects.create(nome=novo_time.nome)
+    jogador = Jogador.objects.create(**novo_jogador._asdict(), time=time)
+    time.delete()
+    jogador.refresh_from_db()
+    assert jogador.time is None
